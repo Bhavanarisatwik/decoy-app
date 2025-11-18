c269be70b7ae0139ca47c59a01986276b1478cb7
Bhavanarisatwik <164032985+Bhavanarisatwik@users.noreply.github.com>
2025-11-18 21:38:33 +0530

    Implement admin module with routing, login, and RBAC components

---
+++ src/app/admin/rbac/rbac.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rbac',
+  imports: [],
+  templateUrl: './rbac.html',
+  styleUrl: './rbac.scss',
+})
+export class Rbac {
+
+}
